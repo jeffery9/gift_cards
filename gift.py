2d28a3df70cc057c5fbaaaf465564af209b08a22
Ryan Casey <ryepdx@gmail.com>
2014-02-22 15:06:42 -0800

    Gift cards are no longer associated with companies. They also are now automatically assigned unique 16-character (19 if you count the spaces) IDs upon creation.

--- gift.py
+++ gift.py
@@ -1,10 +1,23 @@
+import re
+from helpers.shortuuid import uuid
 from openerp.osv import fields, osv
 
 class card(osv.osv):
     _name = "gift.card"
+    _rec_name = "number"
     _columns = {
-        'value': fields.float('Card Value'),
-        'partner_id': fields.many2one("res.partner", 'Customer'),
+        'number': fields.char('Card Number', size=19, readonly=True),
+        'value': fields.float('Card Value')
     }
+    _defaults = {
+        # Creates a random string of 16 characters,
+        # with a space between every group of 4.
+        'number': lambda *args: ' '.join(
+            re.findall('....', uuid().upper()[0:16])
+        )
+    }
+    _sql_constraints = [
+        ('number_unique', 'unique(number)', 'Card number collision! Try again.')
+    ]
 
 card()

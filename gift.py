77800d7f3dd3d0e3e963fdd78044ae4ab6b7e600
Ryan Casey <ryepdx@gmail.com>
2014-03-08 16:48:57 -0800

    Added comments.

--- gift.py
+++ gift.py
@@ -3,6 +3,8 @@ from helpers.shortuuid import uuid
 from openerp.osv import fields, osv
 
 class card(osv.osv):
+    '''Represents a gift card issued to a customer.'''
+
     _name = "gift.card"
     _rec_name = "number"
     _columns = {
@@ -17,6 +19,10 @@ class card(osv.osv):
         )
     }
     _sql_constraints = [
+        # This constraint is highly unlikely to be violated, given the fact
+        # that `number` is based off a UUID. If it gets repeatedly violated,
+        # beware! There is probably some sort of dark magic afoot. Or else you
+        # just have way too many damn gift cards in your database.
         ('number_unique', 'unique(number)', 'Card number collision! Try again.')
     ]
 
